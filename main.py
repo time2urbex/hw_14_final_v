7ec39adac1fb7972bc4230d98d31f99c1fc2ed4d
time2urbex <70639041+time2urbex@users.noreply.github.com>
2022-08-09 04:03:37 +0300

    123выаasd

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from flask import Blueprint, render_template, request, Flask, jsonify, redirect
-
+from utils import *
 from utils import get_movie_by_name
 app = Flask(__name__)
 
 
 
-@app.route("/movie/<title>")
-def page_movies(movie_id):
-    movie = get_movie_by_name(name)
+@app.route("/movie/<title_name>")
+def page_by_name(movie_id):
+    movie = get_movie_by_name(title_name)
     render_template('by_name.html', movie=movie)
 
+@app.route("/movie/<year1>/to/<year2>")
+def page_by_years(year1, year2):
+
+    movie = get_movie_by_year(year1,year2)
+    render_template('by_year.html', movie=movie)
+
+@app.route("/rating/<rating_name>")
+def get_rating(rating_name):
+
+    movie = search_by_raiting(rating_name):
+    render_template('raiting.html', movie=movie)
 
 
 
 app.run(port=3035, debug=True)
+
 
 """ 
 def to_jsonify():
--- utils.py
+++ utils.py
@@ -4,17 +4,68 @@ import sqlite3
 def get_movie_by_name(name):
     con = sqlite3.connect("netflix.db")
     cur = con.cursor()
-    sqlite_query = ("SELECT `title`, `release_year` FROM netflix "
-                    "WHERE release_year BETWEEN 1943 AND 1945 "
+    sqlite_query = ("SELECT `title`,`country`,`release_year`,`listed_in`,`description` FROM netflix "
+                    "WHERE `title` LIKE title_name "
                     "AND type='Movie'")
-
 
     result = cur.execute(sqlite_query)
     data = cur.fetchone()  # С помощью этой функции получаем результат запроса в виде списка кортежей
     con.close()  # После выполнения запросов обязательно закрываем соединение с БД
     return data[0], data[1]
 
+def convert_to_jsonify():
+    for listed_in in get_movie_by_name:
+        title=listed_in
+    return title
 
+
+def get_movie_by_year(year):
+    con = sqlite3.connect("netflix.db")
+    cur = con.cursor()
+    sqlite_query2 = ("SELECT `title`, `release_year` FROM netflix " \
+                     "WHERE release_year BETWEEN year1 AND year2 " \
+                     "ORDER BY `release_year` " \
+                     "LIMIT 100"\
+                     "AND type='Movie'")
+
+
+    result = cur.execute(sqlite_query)
+    mytable = prettytable.from_db_cursor(result)
+    mytable.max_width = 30
+
+    result = cur.execute(sqlite_query2)
+    data = cur.fetchone()  # С помощью этой функции получаем результат запроса в виде списка кортежей
+    con.close()  # После выполнения запросов обязательно закрываем соединение с БД
+    return data[0], data[1]
+
+
+
+def get_movie_by_raiting(year):
+    con = sqlite3.connect("netflix.db")
+    cur = con.cursor()
+    sqlite_query3 = ("SELECT `title`, `rating`, `description` FROM netflix " \
+                     "WHERE rating == raiting_name" \
+                     "ORDER BY `rating` " \
+                     "LIMIT 10"\
+                     "AND type='Movie'")
+
+    result = cur.execute(sqlite_query)
+    mytable = prettytable.from_db_cursor(result)
+    mytable.max_width = 30
+
+    result = cur.execute(sqlite_query2)
+    data = cur.fetchone()  # С помощью этой функции получаем результат запроса в виде списка кортежей
+    con.close()  # После выполнения запросов обязательно закрываем соединение с БД
+    return data[0], data[1]
+
+def search_by_raiting(rating_name):
+
+    for rating in data:
+        children = "G"
+        family = "G", "PG", "PG-13"
+        adult = "R", "NC-17"
+        if rating == "rating_name":
+            print(data)
 
 
 """
